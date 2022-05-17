ddb0d427f66493373f6f64bfc6f2e100f480b1d0
Plippy12 <dalebrown@live.co.uk>
2022-05-17 21:02:13 +0200

    JSON reader for Tuned

--- main.py
+++ main.py
@@ -3,13 +3,33 @@ from altair.examples.ranged_dot_plot import chart
 import json
 import altair as alt
 from altair import pipe, limit_rows, to_values
+import streamlit as st
+
+st.set_page_config(page_title='Dashboard', page_icon="🔌", layout='wide', initial_sidebar_state='expanded')
 
 alt.data_transformers.register('custom', lambda data: pipe(data, limit_rows(max_rows=10000), to_values))
 alt.data_transformers.enable('custom')
 alt.renderers.enable('altair_viewer')
 
-with open('json/json.json', encoding='utf-8-sig') as f:
-    json_data = json.load(f)
+
+def json_load(json_data1):
+    with open(json_data1, encoding='utf-8-sig') as f:
+        json_data1 = json.load(f)
+
+    return json_data1
+
+
+uploaded_file = st.file_uploader("Choose a file", type=['json'])
+
+
+# if uploaded_file is not None:
+#     json_load(uploaded_file)
+#
+# json_data = uploaded_file
+# with open('json/json.json', encoding='utf-8-sig') as f:
+#     json_data = json.load(f)
+
+json_data = json.load(uploaded_file)
 
 data1 = pd.json_normalize(json_data['trades'], record_path=['orders'], meta=['tradeNo'])
 data2 = pd.json_normalize(json_data, record_path=['trades'])
@@ -72,8 +92,7 @@ merged["cumProf"] = merged.apply(lambda x: get_profit(x['cumBal'], x['startAlloc
 # print("data3 is:", data3.keys())
 # print("filtered is:", filtered.keys())
 print("merged is:", merged.keys())
-merged.to_csv('Trade-Data.csv')
-result.to_csv('Monthly-Data.csv')
+
 
 
 coin = 'na'
@@ -101,17 +120,15 @@ chart.properties(width=700).configure_axisY(
 )
 
 
-# merged['compoundProfitPerc100'] = merged['compoundProfitPerc'] * 100.0
 merged['maxValPerc'] = merged['cumProf'].max()
 merged['minValPerc'] = merged['cumProf'].min()
-
 
 y_range_max_1 = merged['maxValPerc'].max()
 y_range_min_1 = merged['minValPerc'].min()
 
 x_range_max_1 = merged['filledTime'].max()
 x_range_min_1 = merged['filledTime'].min()
-#
+
 merged['maxValBal'] = merged["cumBal"].max()
 merged['minValBal'] = merged["cumBal"].min()
 
@@ -131,81 +148,85 @@ nearest1 = alt.selection(type='single', nearest=True, on='mouseover',
 
 result.reset_index(inplace=True)
 result['monthYear'] = "01" + "-" + result["filledTimeM"].astype(str) + "-" + result["filledTime"].astype(str)
+result['profit1'] = result['profit'] * 100.0
 print(result)
 print(result.keys())
 
+merged.to_csv('Trade-Data.csv')
+result.to_csv('Monthly-Data.csv')
+
 bars = alt.Chart(result).mark_bar().encode(
-    x=alt.X('monthYear:O', scale=alt.Scale(nice=False),
+    x=alt.X('monthYear:O', sort=alt.EncodingSortField(field="monthYear", op='count', order='ascending'),
+            scale=alt.Scale(nice=False),
             axis=alt.Axis(formatType="timeUnit", title='Date')),
-    y=alt.Y('profit', scale=alt.Scale(nice=False),
-            axis=alt.Axis(title=f'Accumulated % of {startAlloc[0]} {coin[1]} Per Month', grid=True, format='%',
+    y=alt.Y('profit1', scale=alt.Scale(nice=False),
+            axis=alt.Axis(title=f'Accumulated % of {startAlloc[0]} {coin[1]} Per Month', grid=True, #, format='%.2f'
                           offset=0))
 )
 
 
+chart = alt.Chart(merged).mark_line(
+    interpolate='basis',
+    line={'color': 'yellow'},
+    opacity=0.5
+    ).encode(
+    x=alt.X('filledTime:T', scale=alt.Scale(nice=False),
+            axis=alt.Axis(formatType="timeUnit", format="%B of %Y", title='Date',
+                          labelAngle=-70,
+                          labelSeparation=3,
+                          labelPadding=0,
+                          labelOverlap=True)),
+    y=alt.Y('cumProf', scale=alt.Scale(nice=False),
+            axis=alt.Axis(labelSeparation=3,
+                          labelPadding=0,
+                          labelOverlap=True)),
+).properties(
+    title=f'{titleData["name"][0]} - {titleData["type"][0]} - Trading {coinData["coinPair"][0]}'
+)
 
-# chart = alt.Chart(merged).mark_line(
-#     interpolate='basis',
-#     line={'color': 'yellow'},
-#     opacity=0.5
-#     ).encode(
-#     x=alt.X('filledTime:T', scale=alt.Scale(nice=False),
-#             axis=alt.Axis(formatType="timeUnit", format="%B of %Y", title='Date',
-#                           labelAngle=-70,
-#                           labelSeparation=3,
-#                           labelPadding=0,
-#                           labelOverlap=True)),
-#     y=alt.Y('cumProf', scale=alt.Scale(nice=False),
-#             axis=alt.Axis(labelSeparation=3,
-#                           labelPadding=0,
-#                           labelOverlap=True)),
-# ).properties(
-#     title=f'{titleData["name"][0]} - {titleData["type"][0]} - Trading {coinData["coinPair"][0]}'
-# )
-#
-#
-# selectors = alt.Chart(merged).mark_point().encode(
-#     x='filledTime:T',
-#     opacity=alt.value(0),
-# ).add_selection(
-#     nearest
-# )
-#
-#
-# # Draw points on the line, and highlight based on selection
-# points = chart.mark_point().encode(
-#     opacity=alt.condition(nearest, alt.value(1), alt.value(0))
-# )
-#
-# # Draw text labels near the points, and highlight based on selection
-# text = chart.mark_text(align='left', dx=5, dy=-5).encode(
-#     text=alt.condition(nearest, 'cumProf', alt.value(' '))
-# )
-#
-# # Draw a rule at the location of the selection
-# rules = alt.Chart(merged).mark_rule(color='gray').encode(
-#     x='filledTime:T',
-# ).transform_filter(
-#     nearest
-# )
-#
-# # Put the five layers into a chart and bind the data
-# plot = alt.layer(
-#     chart.mark_line(color='blue').encode(
-#         y=alt.Y('cumProf', scale=alt.Scale(nice=False),
-#                 axis=alt.Axis(title=f'Accumulated % of {startAlloc[0]} {coin[1]}', grid=True, format='%',
-#                               offset=0))),
-#     selectors,
-#     points,
-#     text,
-#     rules
-# ).properties(
-#     width=800,
-#     height=500
-# )
-#
-#
-# ######
+
+selectors = alt.Chart(merged).mark_point().encode(
+    x='filledTime:T',
+    opacity=alt.value(0),
+).add_selection(
+    nearest
+)
+
+
+# Draw points on the line, and highlight based on selection
+points = chart.mark_point().encode(
+    opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+)
+
+# Draw text labels near the points, and highlight based on selection
+text = chart.mark_text(align='left', dx=5, dy=-5).encode(
+    text=alt.condition(nearest, 'cumProf', alt.value(' '))
+)
+
+# Draw a rule at the location of the selection
+rules = alt.Chart(merged).mark_rule(color='gray').encode(
+    x='filledTime:T',
+).transform_filter(
+    nearest
+)
+
+# Put the five layers into a chart and bind the data
+plot = alt.layer(
+    chart.mark_line(color='blue').encode(
+        y=alt.Y('cumProf', scale=alt.Scale(nice=False),
+                axis=alt.Axis(title=f'Accumulated % of {startAlloc[0]} {coin[1]}', grid=True, format='%',
+                              offset=0))),
+    selectors,
+    points,
+    text,
+    rules
+).properties(
+    width=600,
+    height=400
+)
+
+
+######
 #
 # chart1 = alt.Chart(merged).mark_line(
 #     interpolate='basis',
@@ -265,9 +286,9 @@ bars = alt.Chart(result).mark_bar().encode(
 #     height=300
 # )
 
-plot2 = bars #plot #alt.hconcat(plot, plot1)
-# plot2.save('BacktestData.html')
+plot2 = alt.vconcat(plot, bars)
+plot2.save('BacktestData.html')
 
-plot2.show()
+# plot2.show()
 
-
+st.altair_chart(plot2, use_container_width=True)
