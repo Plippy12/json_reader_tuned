45d493d68c1aaf9de564875efce8bd87d199e9b8
Plippy12 <dalebrown@live.co.uk>
2022-05-17 21:25:26 +0200

    JSON reader for Tuned

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='json_reader_tuned',
+    version='1',
+    packages=['vega_datasets', 'vega_datasets.tests'],
+    url='',
+    license='',
+    author='Dale Brown-Scholte',
+    author_email='dalebrown@live.co.uk',
+    description='JSON Viewer-Tuned'
+)
