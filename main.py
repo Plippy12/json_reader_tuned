ccaa0ac7bc1d273fba1ccda6caa11a9b2b04a39b
Plippy12 <dalebrown@live.co.uk>
2022-05-21 20:11:27 +0200

    JSON reader for Tuned

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ if uploaded_file is not None:
 
     merged["Cumulative_Profit_Max"] = merged.Cumulative_Profit.shift(fill_value=0).cummax()
 
-    merged["Cumulative_Profit_Min"] = np.where((merged["Cumulative_Profit_Max"] != merged["Cumulative_Profit_Max"][1]),
+    merged["Cumulative_Profit_Min"] = np.where((merged["Cumulative_Profit_Max"] < merged["Cumulative_Profit_Max"][1]),
                                                merged.loc[:, ["Cumulative_Profit"]].min(1),
                                                merged.loc[:, ["Cumulative_Profit"]].max(1),
                                                )
