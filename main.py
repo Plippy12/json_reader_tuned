ff46c7a7351632a3b6cb1dd4dde709063604150a
Plippy12 <dalebrown@live.co.uk>
2022-05-22 18:43:29 +0200

    JSON reader for Tuned

--- main.py
+++ main.py
@@ -91,7 +91,10 @@ if uploaded_file is not None:
 
     merged["Cumulative_Profit_Max"] = merged.Cumulative_Profit.shift(fill_value=0).cummax()
 
-    merged['worst_mdd'] = merged.apply(lambda x: (x['Cumulative_Profit_Max'] - x['Cumulative_Profit']) * -1.0, axis=1)
+    merged['worst_mdd'] = merged.apply(lambda x: 0.0 if x['Cumulative_Profit_Max'] ==
+                                                        0 else ((x['Cumulative_Profit_Max'] - x['Cumulative_Profit']) /
+                                                                x['Cumulative_Profit_Max']) * -1.0, axis=1)
+
     merged['worst_mdd'] = merged.worst_mdd.shift(fill_value=0).cummin()
     print(merged['worst_mdd'])
 
