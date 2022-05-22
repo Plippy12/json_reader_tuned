083d2129a8c6c793b10ed695d1e0a88b5cf941f1
Plippy12 <dalebrown@live.co.uk>
2022-05-22 13:56:37 +0200

    JSON reader for Tuned

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ if uploaded_file is not None:
     merged['trade_duration'] = (merged['filledTime'] - merged.filledTime.shift(1)).dt.total_seconds() / 60 / 60
 
     diff = merged["startAlloc"][0]
-    merged["cumBal"] = merged.apply(lambda x: get_cum_bal(x["startAlloc"], x['startAlloc'], x['profit']), axis=1)
+    merged["cumBal"] = merged.apply(lambda x: get_cum_bal(diff, x['startAlloc'], x['profit']), axis=1)
 
     cum_bal_coin = 0
     merged['cumBalCoin'] = merged.apply(lambda x: get_coin_bal(cum_bal_coin, x['cumBal'], x['filledPrice']), axis=1)
