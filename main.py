ec1b95f876b7d4e57236af0a00704007dbd23fcd
Plippy12 <dalebrown@live.co.uk>
2022-09-04 22:11:36 +0200

    Fixed formula

--- main.py
+++ main.py
@@ -424,6 +424,7 @@ def main():
             if check:
                 expander = st.expander(f'If using Coin Futures - Click here to see the '
                                     f'{data2["currencyPairDetails.quote"][1]} Comparisons')
+                expander.st.write(f"{merged['filledPrice']}")
                 expander.altair_chart(chart2, use_container_width=True)
 
             st.altair_chart(chart3, use_container_width=True)
