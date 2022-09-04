Fixed formula

main.py
@@ -80,6 +80,7 @@ def main():
             merged['filledTimeD'] = pd.to_datetime(merged['filledTime'])
             merged['trade_duration'] = (merged['filledTime'] - merged.filledTime.shift(1)).dt.total_seconds() / 60 / 60
             start_alloc = merged["startAlloc"][0]
+            first_filled = merged['filledPrice'][0]
             merged["cumBal"] = merged.apply(lambda x: x["startAlloc"] + x['cumuProf'], axis=1)
             cum_bal_coin = 0
             merged['cumBalCoin'] = merged.apply(lambda x: x['cumBal'] * x['filledPrice'], axis=1)
@@ -127,7 +128,7 @@ def main():
             merged["Cumulative_Profit"] = merged.apply(lambda x: ((x['cumBal'] - x['startAlloc']) / x['startAlloc']), axis=1)
             merged['profitableTradesTot'] = merged['profitableTrades'].cumsum()
             print(start_price)
-            merged['Strategy_Percentage'] = merged.apply(lambda x: (x['filledPrice'][0] * x['startAlloc']) / (x['filledPrice'] * (x['cumBal'] - x['startAlloc'])), axis=1) # (x['filledPrice'] * x['cumBal']) /
+            merged['Strategy_Percentage'] = merged.apply(lambda x: (first_filled * x['startAlloc']) / (x['filledPrice'] * (x['cumBal'] - x['startAlloc'])), axis=1) # (x['filledPrice'] * x['cumBal']) /
             merged['Profitable_Trades_Perc'] = merged.apply(lambda x: x['profitableTradesTot'] / (x['tradeNo']+1), axis=1)
             merged["Cumulative_Profit_Max"] = merged.Cumulative_Profit.shift(fill_value=0).cummax()
             merged["Cumulative_Profit_Min"] = np.where((merged["Cumulative_Profit_Max"] < merged["Cumulative_Profit_Max"][1]),