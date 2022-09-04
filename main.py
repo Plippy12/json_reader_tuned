Fixed formula

main.py
@@ -126,7 +126,7 @@ def main():
             merged["Cumulative_Profit"] = merged.apply(lambda x: ((x['cumBal'] - x['startAlloc']) / x['startAlloc']), axis=1)
             merged['profitableTradesTot'] = merged['profitableTrades'].cumsum()
             print(start_price)
-            merged['Strategy_Percentage'] = merged.apply(lambda x: (x['cumBal'] * x['filledPrice']) / (x['filledPrice'] * (x['cumBal'] - x['startAlloc'])), axis=1) #
+            merged['Strategy_Percentage'] = merged.apply(lambda x: (x['startAlloc'] * x['filledPrice']) / (x['filledPrice'] * (x['cumBal'] - x['startAlloc'])), axis=1) #
             merged['Profitable_Trades_Perc'] = merged.apply(lambda x: x['profitableTradesTot'] / (x['tradeNo']+1), axis=1)
             merged["Cumulative_Profit_Max"] = merged.Cumulative_Profit.shift(fill_value=0).cummax()
             merged["Cumulative_Profit_Min"] = np.where((merged["Cumulative_Profit_Max"] < merged["Cumulative_Profit_Max"][1]),